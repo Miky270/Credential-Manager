fbd8e3a796e11a33626d18d97eef3bd80e459cab
Miky270 <159611876+Miky270@users.noreply.github.com>
2024-05-09 18:50:21 +0200

    Creazione classe Credential
    -Creazione classe Credential
    -Inserimento funzione addCredentials
    -Aggiunta edit e delete
    -Aggiunta case 3 e 4 nel main

--- Base.js
+++ Base.js
@@ -1,4 +1,4 @@
-const prompt=require('prompt-sync')();
+const prompt = require('prompt-sync')();
 
 const credentials = new Map();
 
@@ -12,12 +12,40 @@ class Credential {
     }
 }
 
-function addCredentials(site, username, password, notes) {
-    const credential = new Credential(site, username, password, notes);
-    credentials.set(site, credential);
+class CredentialManager {
+    constructor() {
+        this.lista = new Map();
+    }
+
+    addCredentials(site, username, password, notes) {
+        const credential = new Credential(site, username, password, notes);
+        this.lista.set(site, credential);
+    }
+
+    editCredentials(site, username, password, notes) {
+        if (this.lista.has(site)) {
+            const credential = this.lista.get(site);
+            credential.username = username;
+            credential.password = password;
+            credential.notes = notes;
+            credential.date = new Date().toLocaleString();
+        } else {
+            console.log("Credenziali non trovate per il sito:", site);
+        }
+    }
+
+    deleteCredentials(site) {
+        if (this.lista.has(site)) {
+            this.lista.delete(site);
+        } else {
+            console.log("Credenziali non trovate per il sito:", site);
+        }
+    }
 }
 
 function main() {
+    let credentialManager = new CredentialManager();
+
     while (true) {
         console.log("\nMenu:");
         console.log("1. Aggiungi credenziali");
@@ -31,16 +59,25 @@ function main() {
 
         switch (choice) {
             case "1":
-                addCredentials(
+                credentialManager.addCredentials(
                                 prompt("Inserisci il sito:"),
                                 prompt("Inserisci lo username:"), 
                                 prompt("Inserisci la password:"), 
                                 prompt("Inserisci le note:")
                             );
                 break;
-            case "2": ;break;
-            case "3": ;break;
-            case "4": ;break;
+            case "2": break;
+            case "3":
+                credentialManager.editCredentials(
+                                prompt("Inserisci il sito da modificare:"), 
+                                prompt("Inserisci il nuovo username:"), 
+                                prompt("Inserisci la nuova password:"), 
+                                prompt("Inserisci le nuove note:")
+                            );
+                break;
+            case "4":
+                credentialManager.deleteCredentials(prompt("Inserisci il sito da eliminare:"));
+                break;
             case "5": ;break;
             case "6":
                 console.log("Arrivederci!");
